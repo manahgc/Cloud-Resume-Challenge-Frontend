0166dec0e50c7f55e93646268ac8cefe5f6bb035
manahgc <manahcharles@yahoo.com>
2023-12-03 23:03:40 -0500

    intergrated site views counter api

--- js/script.js
+++ js/script.js
@@ -1,16 +1,12 @@
-document.addEventListener('DOMContentLoaded', function() {
-  // POST method to increment the visitor count
-  var xhr = new XMLHttpRequest();
-  xhr.open('POST', 'https://yk56eavo5f.execute-api.us-east-1.amazonaws.com/prod/count');
-  xhr.send();
-
+document.addEventListener('DOMContentLoaded', async function() {
+ 
   // GET method to retrieve the current visitor count and display it on the webpage
   async function getCurrentCount() {
-    const response = await fetch('https://yk56eavo5f.execute-api.us-east-1.amazonaws.com/prod/count');
+    const response = await fetch('https://qeelt38opg.execute-api.us-east-1.amazonaws.com/prod/views');
     const data = await response.json();
-    const count = data.count;
-    const countElement = document.getElementById('count');
-    countElement.innerText = count;
+    const count = data.views;
+    const countElement = document.getElementById('views');
+    countElement.innerText = count; 
   }
 
   getCurrentCount();
